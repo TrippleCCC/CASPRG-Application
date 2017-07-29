5614d40636fd6eb8d079796f5fd0e4f861d15f6d
TrippleCCC <chukwurahnonso@gmail.com>
2017-07-28 20:35:52 -0500

    ss

--- Proposal/app.js
+++ Proposal/app.js
@@ -18,7 +18,7 @@ function initalize() {
   var proposal = new Proposal(Proposalquestions);
 
   var element = document.getElementById("Question");
-  element.innerHTML = "getmoney";
+  element.innerHTML = proposal.getQuestion().text;
 
   var nbut = document.getElementById("btn1");
   var bbut = document.getElementById("btn0");
